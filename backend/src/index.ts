bc17caeb6aa14bdffe0e332259c34760ac59ea96
sahil-adhav <sahiladhav16@gmail.com>
2024-05-09 21:54:35 +0530

    Added routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,29 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+// Signup Page
+app.post('/api/v1/user/signup', (c) => {
+  return c.text('Signup Page!')
+})
+
+// Signin Page
+app.post('/api/v1/user/signin', (c) => {
+  return c.text('Signin Page!')
+})
+
+// Blog
+app.put('/api/v1/blog', (c) => {
+  return c.text('Blog Article!')
+})
+
+// Get Blog Article
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Hello Sahil Adhav!')
+})
+
+// Blog bulk
+app.get('/api/v1/blog/bulk', (c) => {
+  return c.text('Blog List!')
 })
 
 export default app

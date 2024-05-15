a5451e6ac1912913dbbce5e3d3f2587e2f13aef0
sahil-adhav <sahiladhav16@gmail.com>
2024-05-16 01:10:03 +0530

    refactored profile

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,10 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { blogRouter } from "./routes/blog";
 import { userRouter } from "./routes/user";
+import { profileRouter } from "./routes/profile";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { decode, sign, verify } from "hono/jwt";
 
 /**
  * *Bindings is an object that has DATABASE_URL, it basically defines shape of object that Hono will expect to work with.
@@ -16,5 +20,6 @@ const app = new Hono<{
 app.use("/*", cors());
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/blog", blogRouter);
+app.route("/api/v1/profile", profileRouter);
 
 export default app;
---
+++ backend/src/routes/profile.ts
@@ -0,0 +1,63 @@
+import { Hono } from "hono";
+import { decode, sign, verify } from "hono/jwt";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+export const profileRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_TOKEN: string;
+  };
+  Variables: {
+    userId: string;
+  };
+}>();
+
+profileRouter.use("/*", async (c, next) => {
+  const jwt = c.req.header("Authorization") || "";
+
+  try {
+    const user = await verify(jwt, c.env.JWT_TOKEN);
+
+    if (user) {
+      c.set("userId", user.id);
+      await next();
+    } else {
+      c.status(403);
+      return c.json({ error: "Please log in to view blogs!" });
+    }
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: "Please log in to view blogs!" });
+  }
+});
+
+profileRouter.get("/", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const jwt = c.req.header("Authorization") || "";
+  const user = await verify(jwt, c.env.JWT_TOKEN);
+  const blogs = await prisma.user.findFirst({
+    where: {
+      id: user.id,
+    },
+    select: {
+      posts: {
+        select: {
+          id: true,
+          title: true,
+          content: true,
+          author: {
+            select: {
+              username: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  return c.json({ blogs });
+});
